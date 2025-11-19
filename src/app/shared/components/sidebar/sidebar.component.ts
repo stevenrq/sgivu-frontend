eac404bef5c5af6da61a88466629b5c1987aa503
Steven <stevenrq8@gmail.com>
2025-11-19 17:07:57 -0500

    feat: agregar responsividad a la barra lateral

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,4 +8,57 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent implements OnInit {
+  private readonly desktopBreakpoint = 992;
+  isMobileView = false;
+  isSidebarOpen = true;
+
+  constructor(@Inject(PLATFORM_ID) private readonly platformId: object) {}
+
+  ngOnInit(): void {
+    this.updateResponsiveState();
+  }
+
+  @HostListener('window:resize')
+  onWindowResize(): void {
+    this.updateResponsiveState();
+  }
+
+  toggleSidebar(): void {
+    if (!this.isMobileView) {
+      return;
+    }
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar(): void {
+    if (!this.isMobileView) {
+      return;
+    }
+    this.isSidebarOpen = false;
+  }
+
+  handleNavigation(): void {
+    this.closeSidebar();
+  }
+
+  private updateResponsiveState(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      this.isMobileView = false;
+      this.isSidebarOpen = true;
+      return;
+    }
+
+    const wasMobileView = this.isMobileView;
+    const mobileView = window.innerWidth < this.desktopBreakpoint;
+    this.isMobileView = mobileView;
+
+    if (mobileView) {
+      if (!wasMobileView) {
+        this.isSidebarOpen = false;
+      }
+    } else {
+      this.isSidebarOpen = true;
+    }
+  }
+}
